574382b4e4c267487f40c74a185d1e6f5bcbb5e8
hekki <contact@hekki.info>
2020-02-27 11:21:23 +0900

    Add aws cli completion

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,6 @@ set -gx PYENV_SHELL fish
 
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
+
+# awscli completion
+#complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (commandline); /usr/local/bin/aws_completer; end)'
