ce114632395c85494a941a563f07df1aa0aef237
hekki <contact@hekki.info>
2020-06-17 17:00:39 +0900

    Add spacefish options

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,5 +8,13 @@ set -gx PYENV_SHELL fish
 # rbenv
 status --is-interactive; and source (rbenv init -|psub)
 
+# spacefish
+set SPACEFISH_KUBECONTEXT_SHOW false
+set SPACEFISH_PACKAGE_SHOW false
+set SPACEFISH_NODE_SHOW false
+set SPACEFISH_DOCKER_SHOW false
+set SPACEFISH_RUBY_SHOW false
+
 # awscli completion
 #complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (commandline); /usr/local/bin/aws_completer; end)'
+
