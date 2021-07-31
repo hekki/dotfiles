e16f5a33b79bebbd04605e5d4a32d32b55a0fa04
hekki <contact@hekki.info>
2021-08-01 03:50:35 +0900

    tmuxのステータスバーを変更

--- bin/install.sh
+++ bin/install.sh
@@ -11,6 +11,13 @@ cp "${BASE_DIR}/tmux.conf" ~/.tmux.conf
 # https://github.com/tmux-plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# for status bar
+curl -o /usr/local/bin/wifi https://raw.githubusercontent.com/b4b4r07/dotfiles/master/.tmux/bin/wifi
+chmod 755 /usr/local/bin/wifi
+
+curl -o /usr/local/bin/battery https://raw.githubusercontent.com/b4b4r07/dotfiles/master/.tmux/bin/battery
+chmod 755 /usr/local/bin/battery
+
 #
 # vim
 #
